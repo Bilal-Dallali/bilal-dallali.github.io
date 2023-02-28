602de206f796a689e9064213d2f747208e7921c3
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2023-02-28 11:18:28 +0100

    starting navbar responsive js

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuBurger = document.querySelector(".burger")
+
+const navLinks = document.querySelector(".links")
+
+menuBurger.addEventListener("click", () => {
+    navLinks.classList.toggle("mobile-menu")
+})
